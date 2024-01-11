6a1e0cfd6375a005ec339280b9cab1ff6f49bc02
David N <nguyen.dddavid@hotmail.com>
2024-01-10 20:06:46 -0500

    Added mouse handling for color changes

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let isMousePressed = true;
     const gridContainer = document.querySelector('.grid-container');
 
     for (let i = 0; i < 16; i++) {
-        for (let j = 0; j < 16; j++){
+        for (let j = 0; j < 16; j++) {
             const gridItem = document.createElement('div');
             gridItem.classList.add('grid-item');
             gridContainer.appendChild(gridItem);
         }
     }
+
+    function changeColor(e) {
+        if (isMousePressed)
+            e.target.style.backgroundColor = 'black';
+    }
+
+    function handleMouseUp() {
+        isMousePressed = false;
+    }
+
+    function handleMouseDown() {
+        isMousePressed = true;
+    }
+
+    function handleMouseMove(e) {
+        if (isMousePressed)
+            changeColor(e);
+    }
+
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(gridItem => gridItem.addEventListener('mousedown', ));
 });
